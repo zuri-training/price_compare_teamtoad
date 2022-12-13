7e9c9ea7c394126748ea31df0a5f1a661536a378
Aim <bintghazall@gmail.com>
2022-12-13 13:50:58 +0100

    adding single product page files

---
+++ price_compare/look/static/js/product_details.js
@@ -0,0 +1,21 @@
+// facebook url 
+let shareOnFacebook = ()=>{
+    const navUrl = 'https://www.facebook.com/sharer/sharer.php?u=' + 'https://www.jumia.com.ng/generic-lebaiqi-over-ear-powerful-bass-stereo-bluetooth-4.0-headphone-handsfree-wireless-headset-with-microphone-fm-radio-micro-sd-card-slot-19145560.html';
+    window.open(navUrl , '_blank');
+  }
+
+  // share via facebook
+shares.forEach(share => {
+    share.addEventListener('click', () => {
+        shareOnFacebook()
+    })
+})
+
+// posting comments
+let commentBody = document.querySelectorAll(".user-comment ul")
+let comment = document.querySelector("#comment")
+let commentButton = document.querySelector([data-comment])
+
+commentButton.addEventListener('click', () =>{
+    commentBody.appendchild() = comment.value
+})
